810dce1cc36be46b849e6c0fe7ef1915a8e85522
mateuszgabrynowicz <mateusz.gabrynowicz@brightinventions.pl>
2024-03-19 15:38:20 +0100

    Clone test

--- step.sh
+++ step.sh
@@ -64,9 +64,19 @@ EOF
     echo ""
 
     echo "Run openvpn"
-      sudo openvpn --daemon --config "vpn_profile.ovpn" --auth-user-pass "credentials.txt" > "$log_path" 2>&1 &
+      sudo openvpn --config "vpn_profile.ovpn" --auth-user-pass "credentials.txt" > "$log_path" 2>&1 &
     echo "Done"
     echo ""
+
+    git "init"
+    git "remote" "add" "origin" "$GIT_REPOSITORY_URL"
+    git "config" "gc.auto" "0"
+
+    git "clean" "-fd"
+    Removing vpn_profile.ovpn
+    git "reset" "--hard" "HEAD"
+
+    git "fetch" "--jobs=10" "--depth=1" "--no-tags" "origin" "refs/heads/develop"
 
     echo "Check status"
     sleep 5
